c8927810a25bebe424331fde55998376d09d671d
gabrielsahdo <gabrielsahdo@gmail.com>
2024-10-24 13:39:59 -0400

    Creating product repository tests

--- ddd/src/infrastructure/repository/product.repository.test.ts
+++ ddd/src/infrastructure/repository/product.repository.test.ts
@@ -1,9 +1,10 @@
 import { afterEach, beforeEach, describe, it } from "jsr:@std/testing/bdd";
 import { assertEquals } from "jsr:@std/assert";
 
-import "npm:sqlite3";
 import { Sequelize } from "npm:sequelize-typescript";
 import ProductModel from "../db/sequelize/model/product.model.ts";
+import Product from "../../domain/entity/product.ts";
+import ProductRepository from "./product.repository.ts";
 
 describe("ProductRepository test", () => {
     let sequelize: Sequelize;
@@ -24,27 +25,65 @@ describe("ProductRepository test", () => {
         await sequelize.close();
     });
 
-    it("test1", async () => {
-        await ProductModel.create({
-            id: "id1",
-            name: "Gabriel",
-            price: 10,
-        })
+    it("should create a product", async () => {
+        const repo = new ProductRepository();
+        const product = new Product("1", "Product 1", 100);
 
-        const product = await ProductModel.findOne();
+        await repo.create(product);
 
-        assertEquals(product?.name, "Gabriel");
+        const productDB = await ProductModel.findByPk(product.id);
+        
+        console.log(product, productDB?.toJSON())
+        assertEquals(productDB?.toJSON(), {
+            id: product.id,
+            name: product.name,
+            price: product.price,
+        });
     });
 
-    it("test2", async () => {
-        await ProductModel.create({
-            id: "id1",
-            name: "Gabriel1",
-            price: 10,
-        })
+    it("should update a product", async () => {
+        const repo = new ProductRepository();
+        const product = new Product("1", "Product 1", 100);
 
-        const product = await ProductModel.findOne();
+        await repo.create(product);
 
-        assertEquals(product?.name, "Gabriel1");
+        product.changeName("Product changed");
+        product.changePrice(200);
+
+        await repo.update(product);
+
+        const found = await ProductModel.findByPk(product.id);
+
+        assertEquals(found?.toJSON(), {
+            id: product.id,
+            name: product.name,
+            price: product.price,
+        });
+    });
+
+    it("should find a product", async () => {
+        const repo = new ProductRepository();
+        const product = new Product("1", "Product 1", 100);
+
+        await repo.create(product);
+        const found = await repo.find(product.id);
+
+        assertEquals(found, product);
+    });
+
+    it("should find all products", async () => {
+        const repo = new ProductRepository();
+
+        const p1 = new Product("1", "Product 1", 100);
+        await repo.create(p1);
+
+        const p2 = new Product("2", "Product 2", 200);
+        await repo.create(p2);
+
+        const foundProducts = await repo.findAll();
+
+        assertEquals(foundProducts.length, 2);
+        assertEquals(foundProducts[0], p1);
+        assertEquals(foundProducts[1], p2);
     });
 });
---
+++ ddd/src/infrastructure/repository/product.repository.ts
@@ -0,0 +1,39 @@
+import Product from "../../domain/entity/product.ts";
+import type ProductRepositoryInterface from "../../domain/repository/product.repository.interface.ts";
+import ProductModel from "../db/sequelize/model/product.model.ts";
+
+export default class ProductRepository implements ProductRepositoryInterface {
+    async create(entity: Product): Promise<void> {
+        await ProductModel.create({
+            id: entity.id,
+            name: entity.name,
+            price: entity.price,
+        });
+    }
+
+    async update(entity: Product): Promise<void> {
+        await ProductModel.update(
+            {
+                name: entity.name,
+                price: entity.price,
+            },
+            {
+                where: { id: entity.id },
+            },
+        );
+    }
+
+    async find(id: string): Promise<Product> {
+        const dbProduct = await ProductModel.findByPk(id);
+
+        if (!dbProduct) throw new Error("Product Not Found");
+
+        return new Product(dbProduct.id, dbProduct.name, dbProduct.price);
+    }
+
+    async findAll(): Promise<Product[]> {
+        const products = await ProductModel.findAll();
+
+        return products.map((p) => new Product(p.id, p.name, p.price));
+    }
+}
